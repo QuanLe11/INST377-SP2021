154a191f328f199b4a7fb4ea5cc55ba087de5bde
QuanLe11 <quanle61500@gmail.com>
2021-02-10 22:56:12 -0500

    Progress #2

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,27 +1,33 @@
 /* Put your javascript in here */
-const slideArray = [];
-for (let i = 0; i < document.querySelectorAll('.slider div').length; i++) {
-    slideArray.push(document.querySelectorAll('.slider div')[i].dataset.background);
-}
+  /* label the images, just for convenience, to visually track them */
+  let i = 1;
+  for(let li of carousel.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+  }
 
-let currentSlideIndex = -1;
+  /* configuration */
+  let width = 130; // image width
+  let count = 3; // visible images count
 
-function advanceSliderItem() {
-    currentSlideIndex++;
+  let list = carousel.querySelector('ul');
+  let listElems = carousel.querySelectorAll('li');
 
-    if (currentSlideIndex >= slideArray.length) {
-        currentSlideIndex = 0;
-    }
+  let position = 0; // ribbon scroll position
 
-    document.querySelector('.slider').style.cssText = 'background: url("' + slideArray[currentSlideIndex] + '") no-repeat center center; background-size: cover;';
+  carousel.querySelector('.prev').onclick = function() {
+      // shift left
+      position += width * count;
+      // can't move to the left too much, end of images
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+  };
 
-    const elems = document.getElementsByClassName('caption');
-    for (let i = 0; i < elems.length; i++) {
-        elems[i].style.cssText = 'opacity: 0;';
-    }
-
-    const currentCaption = document.querySelector('.caption-' + (currentSlideIndex));
-        currentCaption.style.cssText = 'opacity: 1;';
-    }
-
-    let intervalID = setInterval(advanceSliderItem, 3000);
+  carousel.querySelector('.next').onclick = function() {
+      // shift right
+      position -= width * count;
+      // can only shift the ribbbon for (total ribbon length - visible count) images
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+  };
