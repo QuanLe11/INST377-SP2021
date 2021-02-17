26938f7976f8ddde9b5d57a1db5cfe93a9be18d4
QuanLe11 <quanle61500@gmail.com>
2021-02-17 13:49:03 -0500

    Progress #12 Lab 4

--- server.js
+++ server.js
@@ -44,9 +44,8 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
-    res.json({message: 'Hello World'});
-    res.status(200).send('Hello World');
-    // res.send('Hello World');
+    // res.json({message: 'Hello World'});
+    res.send('Hello World');
     console.log('hello123');
   });
 
