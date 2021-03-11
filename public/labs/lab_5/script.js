800a65f55bd1b7cc5b8bc72fc7dda1016d6820a4
QuanLe11 <quanle61500@gmail.com>
2021-03-11 18:45:27 -0500

    Finished Lab 5: Was Able to Have My Top Five Suggestions Appear

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -11,25 +11,6 @@ function mapInit() {
     accessToken: 'pk.eyJ1IjoicWxlMTEiLCJhIjoiY2ttMjYxeWVkNGxiMzJ3cG1mdzh0aHZ2NiJ9.8h8ZiZzj7VUBkXQaMeOdQw'
   }).addTo(mymap);
 
-  const marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-  const circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-  }).addTo(mymap);
-
-  const polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-  ]).addTo(mymap);
-
-  marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
-  circle.bindPopup('I am a circle.');
-  polygon.bindPopup('I am a polygon.');
-
   const popup = L.popup();
 
   function onMapClick(e) {
@@ -49,49 +30,47 @@ async function dataHandler(mapObjectFromFunction) {
   // and target mapObjectFromFunction to attach markers
 
   // API PG County Food Inspection data
-  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const searchInput = document.querySelector('#input');
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // change to '/api'
+  const search = document.querySelector('#input');
+  const formElem = document.querySelector('#formElem');
   const suggestions = document.querySelector('.suggestions');
+  const replyMessage = document.querySelector('.reply-message');
 
   // fetch request
   const request = await fetch(endpoint);
   // empty array for data
-  const zipcodes = await request.json();
-
-  // check for matches using input box compared to zipcodes array
-  /* function findMatches(wordToMatch, zipcodes) {
-    return zipcodes.filter((place) => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return place.zip.match(regex);
-    });
-  } */
-
-  const filtered = data.filter((record) => record.zip.includes(zipcodes.value) && record.geocoded_column_1);
-  const topFive = filtered.slice(0, 5);
+  const data = await request.json();
 
   // display matches found
-  function displayMatches(event) {
+  function displayMatches(event, zipcodes) {
+    // check for matches using input box compared to zipcodes array
+    function findMatches(wordToMatch) {
+      return zipcodes.filter((place) => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.zip.match(regex);
+      });
+    }
+
     const matchArray = findMatches(event.target.value, zipcodes);
 
-    topFive.forEach((item) => {
+    zipcodes.forEach((item) => {
       const longLat = item.geocoded_column_1.coordinates;
       console.log('markerLongLat', longLat[0], longLat[1]);
       const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
 
       const html = matchArray.map((place) => {
         const regex = new RegExp(this.value, 'gi');
-        // const cityName = place.city;
-        // const zipCode = place.zip;
+        const cityName = place.city;
+        const zipCode = place.zip;
         const addressLine1 = place.address_line_1;
         const addressLine2 = place.address_line_2;
         const restaurantName = place.name;
-        // const inspectionResults = place.inspection_results;
-        // const {category} = place;
         return `
               <div class="box is-small">
                   <li>
                       <div class="name">${restaurantName}</div>
                       <address class="address">${addressLine1}, ${addressLine2}</address>
+                      <address class="address">${cityName}, ${zipCode}</address>
                   </li>
               </div>
               `;
@@ -101,9 +80,18 @@ async function dataHandler(mapObjectFromFunction) {
   }
 
   // event listeners
-  searchInput.addEventListener('input', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => {
-    displayMatches(evt);
+  formElem.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    // eslint-disable-next-line max-len
+    const filtered = data.filter((record) => record.zip.includes(search.value) && record.geocoded_column_1);
+    const topFive = filtered.slice(0, 5);
+
+    if (topFive.length < 1) {
+      replyMessage.classList.add('box');
+      replyMessage.innerText = 'No matches found';
+    }
+
+    displayMatches(evt, topFive);
   });
 }
 
